51838a8968f984833bb614dcedca8b429eeceb5b
Aletha Wheeless <augustkama98@gmail.com>
2022-07-20 12:03:45 -1000

    commit tree d694d3fbc6548e9c49b261e359cac0b69f289a3d

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dogs, setDogs] = useState()
+    const [isLoaded, setIsLoaded] = useState(false)
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then((data) => {
+            setDogs(data)
+            setIsLoaded(true)
+        })
+    }, [])
+    if(!isLoaded)return <p>Loading...</p>;
+    return(
+        <img src={dogs.message} alt="A Random Dog"/>
+    )
+}
+export default App;
